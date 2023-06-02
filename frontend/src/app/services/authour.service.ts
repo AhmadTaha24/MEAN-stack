ce98de9053c668efeee988c295904100b638eb9d
yasmen-ahmed <yasmenahmedmuhamed@gmail.com>
2023-06-02 17:42:27 +0300

    edit some function

--- frontend/src/app/services/authour.service.ts
+++ frontend/src/app/services/authour.service.ts
@@ -1,21 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthorResponse } from '../user/components/authours/authour-details/authour-details.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthourService {
 
- 
 
-  constructor(private http:HttpClient){}
 
-  getAuthour(id:number){
+  constructor(private http: HttpClient) { }
+
+  getAuthour(id: number) {
     return this.http.get(`http://localhost:5000/authors/${id}`)
   }
 
-  getAuthourDetails(id:string){
-    return this.http.get(`http://localhost:5000/authors/1/${id}`)
+  getAuthourDetails(id: string): Observable<any> {
+    return this.http.get<any>(`http://localhost:5000/authors/1/${id}`)
 
   }
 }
--- frontend/src/app/user/components/authours/authour-details/authour-details.component.ts
+++ frontend/src/app/user/components/authours/authour-details/authour-details.component.ts
@@ -5,6 +5,8 @@ import { Book } from 'src/app/interfaces/book';
 import { AuthourService } from 'src/app/services/authour.service';
 import { BookService } from 'src/app/services/book.service';
 
+
+export interface AuthorResponse { status: string, data: Authour };
 @Component({
   selector: 'app-authour-details',
   templateUrl: './authour-details.component.html',
@@ -12,40 +14,95 @@ import { BookService } from 'src/app/services/book.service';
 })
 export class AuthourDetailsComponent {
 
-  authours !: Authour[] 
-  Books !: Book[]
+  authours !: Authour[]
+  Books: Book[] = [];
 
   authour !: Authour
-  book !: Book
+  // book !: Book
+
+  id: any
 
 
-  constructor(private activatedRoute:ActivatedRoute,private authourService:AuthourService,private bookService:BookService){}
+  book: any
 
- 
+  // allBooks !: Book[]
 
- 
+
+  constructor(private activatedRoute: ActivatedRoute, private authourService: AuthourService, private bookService: BookService) {
+
+    // this.bookService.getBook(1).subscribe((res:any)=>console.log(res))
+    // console.log(this.Books)
+
+
+  }
+
+
+
+
 
 
   ngOnInit() {
-    let authourId =this.activatedRoute.snapshot.params['id'];
-    // console.log(authourId);
-  
+    // let authourId =this.activatedRoute.snapshot.params['id'];
 
-    this.authourService.getAuthourDetails(authourId).subscribe((res: any) => {
-      // console.log(res);
+    this.activatedRoute.params.subscribe(param => {
+      if (param['id']) {
+        this.authourService.getAuthourDetails(param['id'])
+          .subscribe((user) => {
+            this.authour = user.data?.authour;
+            console
+              .log(this.authour)
+          })
 
-      console.log(res)
-      this.authour = res.data.authour
 
-      console.log(this.authour)
-
-      
-     
+        this.bookService.getBook(1).subscribe(res =>{
+          console.log(res);
+                  })
+      }
     })
+    // console.log(authourId);
+
+
+    // this.authourService.getAuthourDetails(authourId).subscribe((res: any) => {
+    //   // console.log(res);
+
+    //   console.log(res)
+    //   this.authour = res.data.authour
+
+    //   console.log(this.authour)
+
+    //   // console.log(this.authour)
+
+
+    //   // this.id = this.activatedRoute.snapshot.params['id'];
+    //   // // console.log(this.id);
+    //   // this.bookService.getBook(1).subscribe((res:any)=>{
+
+    //   //   this.Books=res
+    //   // const returnValue=  this.Books.filter(book=>book.authorId._id.toLowerCase().includes("647356636ca4714edb7c0fb7".toLowerCase) )
+
+
+    //   //   console.log(returnValue)
+
+
+    //   // }
+    //   // )
+
+
+    // })
+
+
+
+
+
+
+
+
+
+    // this.selectProduct  =this.products.find(product=>product.id == this.activatedRoute.snapshot.params['id'])
 
     // let bookId =this.activatedRoute.snapshot.params['id'];
     // console.log("book id "+bookId);
-    
+
 
     // this.bookService.getBookDetails(bookId).subscribe((res: any) => {
 
@@ -53,12 +110,40 @@ export class AuthourDetailsComponent {
     //   console.log(" book data" +res.data.book)
     // })
 
+    // const returnApiBook = []
 
-  
+    //  while(1){
+
+    //   let count=1
 
 
-    
+    // //  this.allBooks.push(...this.Books)
 
-}
+    //   this.bookService.getBook(1).subscribe((res:any)=>this.Books=res)
+
+    //   if(this.Books==null){
+    //     console.log("break")
+    //     break;
+    //   }
+
+
+    //   console.log(count)
+    //   count++;
+
+
+
+
+    //  }
+
+    //  console.log(this.allBooks)
+
+    // this.id = this.activatedRoute.snapshot.params['id'];
+
+
+
+
+
+
+  }
 
 }
